266cbd7a9b17f6e8513066b390b9d403e8dd1fdf
Braden Byrnes <56095224+Pufferfish3000@users.noreply.github.com>
2023-04-03 11:06:47 -0400

    added file input functions
    - added file input
    - updated gitignore to ignore .txt files

--- Client/Client.c
+++ Client/Client.c
@@ -1,13 +1,15 @@
 #include <stdio.h>
 
 void fileOutput(char* filename, char* text);
+char* fileInput(char* filename);
 
 int main(int argc, char *argv[])
 {
     char* filename = "test.txt";
-    char* text = "test";
+    char* text = "testing \n testing";
     fileOutput(filename, text);
 
+    printf(fileInput(filename));
     return 0;
 }
 
@@ -18,4 +20,32 @@ void fileOutput(char* filename, char* text)
     fprintf(pF, text);
 
     fclose(pF);
+}
+
+char *fileInput(char* filename)
+{
+    FILE *pF = fopen(filename, "r");
+    char *text;
+
+    // Check if file exists
+    if (NULL == pF)
+    {
+        printf("Error opening file");
+    }
+    else
+    {
+        // Get file size
+        fseek(pF, 0, SEEK_END);
+        long size = ftell(pF);
+        // Reset file pointer
+        rewind(pF);
+
+        // Allocate memory for string
+        text = (char*)malloc(size + 1);
+        // Read file into string
+        fread(text, 1, size, pF);
+        // Add null terminator
+        text[size] = '\0';
+    }
+    return text;
 }
