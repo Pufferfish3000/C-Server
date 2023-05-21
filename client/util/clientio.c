31ea696660deeb939df9880a40511ffb63ec6e5a
Braden Byrnes <56095224+Pufferfish3000@users.noreply.github.com>
2023-05-21 15:43:10 -0400

    added documentation to log functions

--- client/util/clientio.c
+++ client/util/clientio.c
@@ -15,41 +15,78 @@
 void outputToFile(char *filename, char *text);
 char *inputFromFile(char *filename);
 
+// node struct for linked list
 struct node
 {
     char *data;
     struct node *next;
 };
 
+// typedef for easier reading when using node struct
 typedef struct node node_t;
 
+// head of linked list
 node_t *head = NULL;
 
+/**
+ * @brief Starts the log
+ * 
+ * This function starts the log by adding a node to the beginning of the linked list,
+ * containing the text "---Start of Log---"
+ * 
+ * @return void
+*/
 void startLog()
 {
+    // allocate memory for node
     node_t *startLog = (node_t *)malloc(sizeof(node_t));
+    // set data and next
     startLog->data = "---Start of Log---\n";
     startLog->next = head;
+    // set startLog as first node
     head = startLog;
 }
 
+/**
+ * @brief Adds a log to the linked list
+ * 
+ * This function adds a log to the linked list by adding a node to the end of the linked list,
+ * 
+ * @param text The text to add to the log
+ * @return void
+*/
 void addLog(char *text)
 {
+    // allocate memory for node
     node_t *newLog = (node_t *)malloc(sizeof(node_t));
+    // set data and next
     newLog->data = text;
     node_t *log = head;
+
+    // iterate through linked list to find last node
     while (log->next != NULL)
     {
         log = log->next;
     }
+    // set last node's next to new node
     log->next = newLog;
 }
 
+/**
+ * @brief Exports the logs to a file
+ * 
+ * This function exports the logs to a file by iterating through the linked list and
+ * outputting each node to a file.
+ * 
+ * @return void
+*/
 void exportLogs()
 {
+    // iterate through linked list
     node_t *log = head;
     while (log != NULL)
     {
+        // append log to file "client.log"
         outputToFile("client.log", log->data);
         log = log->next;
     }
